f8d0ad2398ef7783e729f839a79ebdc3973ffaad
Tiago Pace <tiagopace.dev@gmail.com>
2025-04-02 16:44:59 -0300

    feat: notes editor

---
+++ README.md
@@ -0,0 +1,8 @@
+# Notes
+
+![Screely Image (10)](https://github.com/user-attachments/assets/1c5ad220-fffb-434e-87bf-1cc4c0e2f9d9)
+
+A simple text editor app.
+
+- Supports Markdown.
+- Built with Astro and Tiptap.
---
+++ src/functions/notes.js
@@ -0,0 +1,73 @@
+import { ulid } from 'ulid'
+
+export function getNoteId() {
+  const hash = window.location.hash.substring(1)
+  if (hash) return hash
+
+  const noteId = ulid()
+  window.location.hash = noteId
+  return noteId
+}
+
+function getAllNotes() {
+  const notes = []
+
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i)
+    if (key?.startsWith('note-')) {
+      const noteId = key.replace('note-', '')
+      const content = localStorage.getItem(key) || ''
+
+      const tempDiv = document.createElement('div')
+      tempDiv.innerHTML = content.split('<h1>')[1].split('</h1>')[0].trim().substring(0, 45)
+      const title = tempDiv.textContent || 'New note'
+
+      notes.push({
+        id: noteId,
+        title,
+        content,
+      })
+    }
+  }
+
+  return notes.sort((a, b) => b.id.localeCompare(a.id))
+}
+
+export function updateNotesList() {
+  const notesList = document.getElementById('notes-list')
+  if (!notesList) return
+
+  const noteId = getNoteId()
+  const notes = getAllNotes()
+  notesList.innerHTML = ''
+
+  for (let i = 0; i < notes.length; i++) {
+    const note = notes[i]
+
+    const noteElement = document.createElement('div')
+    noteElement.classList.add('note-item')
+    if (note.id === noteId) {
+      noteElement.classList.add('active')
+    }
+    noteElement.textContent = note.title
+    noteElement.addEventListener('click', () => {
+      window.location.hash = note.id
+      window.location.reload()
+    })
+    notesList.appendChild(noteElement)
+  }
+}
+
+export function removeNote() {
+  const noteId = getNoteId()
+  if (confirm('Are you sure you want to delete this?')) {
+    localStorage.removeItem(`note-${noteId}`)
+    updateNotesList()
+
+    const notes = getAllNotes()
+    if (notes.length > 0) {
+      window.location.hash = notes[0].id
+      window.location.reload()
+    }
+  }
+}
